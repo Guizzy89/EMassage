1f4b8a7e3fbb57ca2830483d789a60cb99fda0fc
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-02 16:09:16 +0300

    Скопировал код  в файл Program.cs из прошлого проекта. Добавил службы аутентификации, т.к. проект в этот раз погдразумевает их наличие.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавляем службы аутентификации
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        opt.LoginPath = "/Account/Login";          // Страничка входа
+        opt.LogoutPath = "/Account/Logout";        // Страничка выхода
+        opt.AccessDeniedPath = "/Account/AccessDenied"; // Страничка отказа в доступе
+    });
+
+builder.Services.AddControllersWithViews();
+
 // Подключение к локальной базе данных SQLite
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=DMassage.db"));
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
 
 // Включаем процессы аутентификации и авторизации
 app.UseAuthentication();
