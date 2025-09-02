9d5730430c0caa3b5c1d7c6273dc49f9d4e9a654
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-02 15:04:53 +0300

    В файле Program.cs прописываем подключение к базе данных // Подключение к локальной базе данных SQLite builder.Services.AddDbContext<ApplicationDbContext>(options =>     options.UseSqlite("Data Source=DMassage.db"));
    ну и просто дополняю код и привожу в порядок

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using EMassage.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Подключение к локальной базе данных SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=DMassage.db"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// Включаем процессы аутентификации и авторизации
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Стандартные middleware
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
