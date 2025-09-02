d410c465fe04c13c2c5010bb5a37efaaef61f27c
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-02 16:11:43 +0300

    в папку Views добавил Shared c файлом _Layout.cshtml его было необходимо добавить сразу же, вместе с Home

---
+++ Views/Shared/_Layuot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EMassage.Views.Shared
+{
+    public class _LayuotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
