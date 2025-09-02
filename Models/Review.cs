cdaadf376985c590ff0af02d477761173031d877
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-02 14:47:33 +0300

    Создал папку Models  куда поместил сущности необходимые для наего проекта - Пользователи, Услуги Массажа, Брони и Отзывы. Связь между сущностями намеренно пока оставил только между Пользоватлем и отзывом один ко многим. Так же важно отметить, что  в сущности Пользователя есть попытка реализовать методжы проверки полномочий, чтобы не зарегистрированный пользователь не мог выбирать брони и оставлять отзывы. При этом так же не обладал полномочиями администратора.

---
+++ Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EMassage.Models
+{
+    public class Review
+    {
+        // Первичный ключ отзыва
+        public int Id { get; set; }
+
+        // Содержимое отзыва
+        [Required]
+        public string Content { get; set; }
+
+        // Дата и время публикации отзыва
+        public DateTime CreatedAt { get; set; }
+
+        // Внешний ключ, связанный с пользователем
+        public int AuthorId { get; set; }
+
+        // Навигационная ссылка на объект пользователя
+        public virtual User Author { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMassage.Models
+{
+    public class User
+    {
+        // Первичный ключ пользователя
+        public int Id { get; set; }
+
+        // Логин пользователя
+        [Required(ErrorMessage = "Необходимо ввести логин.")]
+        public string Login { get; set; }
+
+        // Пароль пользователя (хешированный)
+        [Required(ErrorMessage = "Необходимо ввести пароль.")]
+        public string PasswordHash { get; set; }
+
+        // Роль администратора (true - администратор, false - обычный пользователь)
+        public bool IsAdmin { get; set; } = false;
+
+        // Методы проверки полномочий
+        public bool CanLogin() => !string.IsNullOrEmpty(Login);
+        public bool CanLogout() => this.CanLogin();
+        public bool CanSelectMassageService() => this.CanLogin(); // Только зарегистрированные пользователи могут выбрать услугу
+        public bool CanLeaveReview() => this.CanLogin();          // Только зарегистрированные пользователи могут оставить отзыв
+
+        // Метод изменения статуса администрации (например, при изменении прав пользователя)
+        public void SetAsAdmin(bool isAdmin = true)
+        {
+            this.IsAdmin = isAdmin;
+        }
+    }
+}
